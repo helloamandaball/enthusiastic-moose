d0d048d5aa8158a7b8771b88b81d190c32afa0cf
helloamandaball <helloamandaball@gmail.com>
2022-01-20 11:18:53 -0500

    updated through refactored code as part of phase 8 in chapter

--- Program.cs
+++ Program.cs
@@ -10,28 +10,47 @@ namespace EnthusiasticMoose
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine();
 
-            // // Let the moose speak!
-                // MooseSays("H I, I'M  E N T H U S I A S T I C !");
-                // MooseSays("I really am enthusiastic");
+        // // Let the moose speak!
+            // MooseSays("H I, I'M  E N T H U S I A S T I C !");
+            // MooseSays("I really am enthusiastic");
 
-            // //Ask a question -- moved this method/function to below and called it as CanadaQuestion();
-                // bool isTrue = MooseAsks("Is Canada real?");
-                // if (isTrue)
-                // {
-                //     MooseSays("Really? It seems very unlikely.");
-                // }
-                // else
-                // {
-                //     MooseSays("I  K N E W  I T !!!");
-                // }
-                
-            // Ask some questions
-            CanadaQuestion();
-            EnthusiasticQuestion();
-            LoveCSharpQuestion();
-            SecretQuestion();
+        // //Ask a question -- Ch.3 Phase 6, but moved this method/function to below and called it as CanadaQuestion();
+            // bool isTrue = MooseAsks("Is Canada real?");
+            // if (isTrue)
+            // {
+            //     MooseSays("Really? It seems very unlikely.");
+            // }
+            // else
+            // {
+            //     MooseSays("I  K N E W  I T !!!");
+            // }
+            
+        // Ask some questions - Ch.3 Phase 7
+            // CanadaQuestion();
+            // EnthusiasticQuestion();
+            // LoveCSharpQuestion();
+            // SecretQuestion();
+
+        // Ask some questios - Ch.3 Phase 8 - Refactor code (make it smaller/streamline it)
+            QuestionRefactor("Is Canada real?", "Really? It seems very unlikely.", "I  K N E W  I T !!!");
+            QuestionRefactor("Are you enthusiastic?", "Yay!", "You should try it!");
+            QuestionRefactor("Do you love C# yet?", "Good job sucking up to your instructor!", "You will...oh, yes, you will...");
+            QuestionRefactor("Do you want to know a secret?", "ME TOO!!!! I love secrets...tell me one!", "Oh, no...secrets are the best, I love to share them!");
         }
 
+    // Ch.3 Phase 8 - Refactor code (make it smaller/streamline it)
+        static void QuestionRefactor(string questionToAsk, string ifYes, string ifNo)
+        {
+            bool isTrue = MooseAsks(questionToAsk);
+            if (isTrue) {
+                MooseSays(ifYes);
+            }
+            else {
+                MooseSays(ifNo);
+            }
+        }
+
+    //Starter code for MooseSays question:
         static void MooseSays(string message)
         {
             Console.WriteLine($@"
@@ -85,56 +104,58 @@ namespace EnthusiasticMoose
             }
         }
 
-        static void CanadaQuestion()
-        {
-            bool isTrue = MooseAsks("Is Canada real?");
-            if (isTrue)
-            {
-                MooseSays("Really? It seems very unlikely.");
-            }
-            else
-            {
-                MooseSays("I  K N E W  I T !!!");
-            }
-        }   
 
-        static void EnthusiasticQuestion()
-        {
-            bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-            if (isEnthusiastic)
-            {
-                MooseSays("Yay!");
-            }
-            else
-            {
-                MooseSays("You should try it!");
-            }
-        }
+    // // Ch. 3 Phase 7:
+        // static void CanadaQuestion()
+        // {
+        //     bool isTrue = MooseAsks("Is Canada real?");
+        //     if (isTrue)
+        //     {
+        //         MooseSays("Really? It seems very unlikely.");
+        //     }
+        //     else
+        //     {
+        //         MooseSays("I  K N E W  I T !!!");
+        //     }
+        // }   
 
-        static void LoveCSharpQuestion()
-        {
-            bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-            if (doesLoveCSharp)
-            {
-                MooseSays("Good job sucking up to your instructor!");
-            }
-            else
-            {
-                MooseSays("You will...oh, yes, you will...");
-            }
-        }
+        // static void EnthusiasticQuestion()
+        // {
+        //     bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+        //     if (isEnthusiastic)
+        //     {
+        //         MooseSays("Yay!");
+        //     }
+        //     else
+        //     {
+        //         MooseSays("You should try it!");
+        //     }
+        // }
 
-        static void SecretQuestion()
-        {
-            bool isEverythingFine = MooseAsks("Do you want to know a secret?");
-            if (isEverythingFine)
-            {
-                MooseSays("ME TOO!!!! I love secrets...tell me one!");
-            }
-            else
-            {
-                MooseSays("Oh, no...secrets are the best, I love to share them!");
-            }
-        }
+        // static void LoveCSharpQuestion()
+        // {
+        //     bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+        //     if (doesLoveCSharp)
+        //     {
+        //         MooseSays("Good job sucking up to your instructor!");
+        //     }
+        //     else
+        //     {
+        //         MooseSays("You will...oh, yes, you will...");
+        //     }
+        // }
+
+        // static void SecretQuestion()
+        // {
+        //     bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+        //     if (isEverythingFine)
+        //     {
+        //         MooseSays("ME TOO!!!! I love secrets...tell me one!");
+        //     }
+        //     else
+        //     {
+        //         MooseSays("Oh, no...secrets are the best, I love to share them!");
+        //     }
+        // }
     }
 }
